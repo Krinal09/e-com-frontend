9bb74713107593206b7812c987b03ea4895e33b3
Krinal Gami <104003095+Krinal09@users.noreply.github.com>
2025-04-20 12:03:41 +0530

    Update index.js

--- src/store/auth-slice/index.js
+++ src/store/auth-slice/index.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 const initialState = {
   isAuthenticated: false,
