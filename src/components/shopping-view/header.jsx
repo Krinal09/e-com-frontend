951072e4b9bdfcebe7756e960aa1507cefa2e537
Krinal Gami <104003095+Krinal09@users.noreply.github.com>
2025-04-21 02:00:45 +0530

    Update header.jsx

--- src/components/shopping-view/header.jsx
+++ src/components/shopping-view/header.jsx
@@ -1,4 +1,5 @@
-import { HousePlug, LogOut, Menu, ShoppingCart, UserCog } from "lucide-react";
+import { LogOut, Menu, ShoppingCart, UserCog } from "lucide-react";
+import { FaShoppingBag } from "react-icons/fa";
 import {
   Link,
   useLocation,
@@ -147,8 +148,8 @@ function ShoppingHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
-          <HousePlug className="h-6 w-6" />
-          <span className="font-bold">Ecommerce</span>
+          <FaShoppingBag className="h-6 w-6" />
+          <span className="font-bold">Shopnest</span>
         </Link>
         <Sheet>
           <SheetTrigger asChild>
