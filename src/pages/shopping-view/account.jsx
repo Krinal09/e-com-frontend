a216c77f3973c2fbd298026b94949a915411dfa1
Krinal Gami <104003095+Krinal09@users.noreply.github.com>
2025-04-20 23:09:48 +0530

    Update account.jsx

--- src/pages/shopping-view/account.jsx
+++ src/pages/shopping-view/account.jsx
@@ -1,5 +1,4 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import accImg from "../../assets/account.jpg";
 import Address from "@/components/shopping-view/address";
 import ShoppingOrders from "@/components/shopping-view/orders";
 import ProfileManagement from "@/components/shopping-view/profile-management";
@@ -11,14 +10,18 @@ function ShoppingAccount() {
 
   return (
     <div className="flex flex-col">
-      <div className="relative h-[300px] w-full overflow-hidden">
+      {/* <br />
+      <h1 className="text-2xl font-bold text-center">Account</h1> */}
+      {/* <div className="relative h-[300px] w-full overflow-hidden">
         <img
           src={accImg}
           className="h-full w-full object-cover object-center"
         />
-      </div>
+      </div> */}
       <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
+        
         <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
+        <h1 className="text-4xl font-bold pb-4">Account</h1>
           <Tabs defaultValue={defaultTab}>
             <TabsList>
               <TabsTrigger value="profile">Profile</TabsTrigger>
